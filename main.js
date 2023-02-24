bc8ac533c31a1ac5bd2829742c018f8885a5aeb9
Adam Meza <Adammeza95@gmail.com>
2023-02-24 13:14:01 -0500

    add show saved functionality

--- main.js
+++ main.js
@@ -1,19 +1,54 @@
 var savedBtn = document.querySelector('.js-save');
 var titleInput = document.querySelector('.js-title-input');
 var bodyInput = document.querySelector('.js-body-input');
-var ideaContainer = document.querySelector('.js-idea-container')
-var formContainer = document.querySelector('.js-form')
+var ideaContainer = document.querySelector('.js-idea-container');
+var formContainer = document.querySelector('.js-form');
+var showStarredBtn = document.querySelector('.js-show-btn')
+var showAllBtn = document.querySelector('.js-show-all-btn')
 
 var savedIdeas = [];
 
-formContainer.addEventListener('input', activateSaveBtn)
-ideaContainer.addEventListener('click', starORDelete)
+formContainer.addEventListener('input', activateSaveBtn);
+ideaContainer.addEventListener('click', starORDelete);
+showStarredBtn.addEventListener('click', filterStarred)
+showAllBtn.addEventListener('click', function(){
+    toggleStarORAllBtn("Show All")
+    repopulateIdeaContainer()
+})
+
+function toggleStarORAllBtn(currentState){
+    if (currentState === "Show All"){
+        showAllBtn.classList.add('hidden')
+        showStarredBtn.classList.remove('hidden')
+    } else if (currentState === "Show Starred")
+        showAllBtn.classList.remove('hidden')
+        showStarredBtn.classList.add('hidden')
+}
+
+function filterStarred(){
+    toggleStarORAllBtn("Show Starred")
+    ideaContainer.innerHTML = ''
+    for (var i = 0; i < savedIdeas.length; i++) {
+        if (savedIdeas[i].star){
+            ideaContainer.innerHTML += `
+            <div class="card" id="${savedIdeas[i].id}">
+             <header class="card-header">
+               <img class="star js-star" id="star" src="assests/star-active.svg">
+               <img class="delete js-delete" id="delete" src="assests/delete.svg">
+             </header>
+             <p class="header card-title">${savedIdeas[i].title}</p>
+             <p class="card-text">${savedIdeas[i].body}</p>
+             <div class="comment"></div>
+           </div>`
+        }
+    }
+}
 
 savedBtn.addEventListener('click', function () {
-    createIdea()
-    clearInput()
-    repopulateIdeaContainer()
-    savedBtn.disabled = true
+    createIdea();
+    clearInput();
+    repopulateIdeaContainer();
+    savedBtn.disabled = true;
 })
 
 function createIdea() {
@@ -30,22 +65,28 @@ function clearInput() {
 
 function starORDelete(event) {
     if (event.target.id === 'star') {
-        console.log ('STAR if')
+        savedIdeas[findIndex()].updateIdea(findIndex());
+        repopulateIdeaContainer();
     } else if (event.target.id === 'delete') {
-        savedIdeas[findIndex()].deleteFromStorage(findIndex())
-        repopulateIdeaContainer()
+        savedIdeas[findIndex()].deleteFromStorage(findIndex());
+        repopulateIdeaContainer();
     }
 }
 
 function repopulateIdeaContainer() {
-    event.preventDefault()
+    event.preventDefault();
     ideaContainer.innerHTML = '';
+    var starImage = ''
     for (var i = 0; i < savedIdeas.length; i++) {
-        ////////// Update img star class //////////////////
+        if (savedIdeas[i].star){
+            starImage = "assests/star-active.svg"
+        } else {
+            starImage = "assests/star.svg"
+        }
         ideaContainer.innerHTML += `
             <div class="card" id="${savedIdeas[i].id}">
              <header class="card-header">
-               <img class="star js-star" id="star" src="assests/star.svg">
+               <img class="star js-star" id="star" src="${starImage}">
                <img class="delete js-delete" id="delete" src="assests/delete.svg">
              </header>
              <p class="header card-title">${savedIdeas[i].title}</p>
@@ -62,18 +103,6 @@ function activateSaveBtn() {
         savedBtn.disabled = true;
     }
 }
-
-//iteration 3.1
-// delete button will splice ideaInst out of array
-//capture eventtarget idea cards ID
-// iterate through array to find it using ID
-// splice it out
-// rerender ideas
-/// query selector on idea container
-// if event.garet.className === "delete" then reun the delete funciton
-// if savedIdeas[i].id === event.target.id { spliice(i,1)}
-// run re render 
-// if event.tagret.classname -=== "star"  then run the updateIDea()
 
 function findIndex() {
     for (var i = 0; i < savedIdeas.length; i++) {
